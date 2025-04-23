c50bed391bfa756e37991bfc5f370bce3995aedc
vrobbin3247 <vaibhavgame3247@gmail.com>
2025-04-23 23:13:32 +0530

    login/signup/content retrieval implementation

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import { supabase } from '../lib/supabaseClient'
 
 import ThoughtCard from '../components/ThoughtCard';
